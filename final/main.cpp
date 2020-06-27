84165567dbf6d05406fe6a8c6a7bb4d67ff32abb
Pochenlin513 <pochenlin513@gmail.com>
2020-06-27 17:14:20 +0800

    mission 1 completed

--- final/main.cpp
+++ final/main.cpp
@@ -14,27 +14,37 @@ PwmOut pin8(D8), pin9(D9);
 DigitalIn pin3(D3);
 DigitalIn pin4(D4);
 Serial xbee(D12, D11);
+Serial uart(D1,D0); //tx,rx
 
 BBCar car(pin8, pin9, servo_ticker);
 
+Thread t;
 char* status_log;
-
+void send_log(){
+    while(1){
+        xbee.printf("%s\r\n", status_log);
+        wait_ms(1000);
+    }
+};
 int main(){
     parallax_encoder encoder0(pin3, encoder_ticker_left);
     parallax_encoder encoder1(pin4, encoder_ticker_right);
     encoder0.reset();
     encoder1.reset();    
+    t.start(send_log);
 
+   
     car.goStraight(100);
     status_log = "go Straight";
-    xbee.printf("%s\r\n", status_log);
-    while(encoder1.get_cm()<105) wait_ms(50);
+    //xbee.printf("%s\r\n", status_log);
+    while(encoder1.get_cm()<110) wait_ms(50);
     car.stop();
     wait(0.5);
 
+/*
     car.turn(100,0.3);
     status_log = "turn Left";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     encoder1.reset();
     while(encoder1.get_cm()<1.9*10.2*2*PI/4) wait_ms(50);
     car.stop();
@@ -42,7 +52,7 @@ int main(){
 
     car.goStraight(100);
     status_log = "go Straight";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     encoder1.reset();
     while(encoder1.get_cm()<100) wait_ms(50);
     car.stop();
@@ -50,19 +60,32 @@ int main(){
 
     car.turn(100,0.3);
     status_log = "turn Left";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     encoder1.reset();
     while(encoder1.get_cm()<1.9*10.2*2*PI/4) wait_ms(50);
     car.stop();
 
     //scan 
-    wait(3);
     status_log = "Image Classification";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
+    char s[21];
+    sprintf(s,"image_classification");
+    uart.puts(s);
+
+    while(1){
+        if(uart.readable()){
+            char recv = uart.getc();
+            xbee.printf("The image is: %c\r\n", recv);
+            break;
+        }
+    }
+
+    wait(1);
+
 
     car.turn(100,0.3);
     status_log = "turn Left";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     encoder1.reset();
     while(encoder1.get_cm()<1.9*10.2*2*PI/4) wait_ms(50);
     car.stop();
@@ -70,7 +93,7 @@ int main(){
 
     car.turn(-100,-0.3);
     status_log = "back turning right";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     encoder0.reset();
     while(encoder0.get_cm()<1.5*10.2*2*PI/4) wait_ms(50);
     car.stop();
@@ -79,7 +102,7 @@ int main(){
     car.goStraight(-100);
     encoder1.reset();
     status_log = "reverse parking";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     while(encoder1.get_cm()<25) wait_ms(50);
     car.stop();
 
@@ -88,22 +111,22 @@ int main(){
     car.goStraight(100);
     encoder1.reset();
     status_log = "go Straight";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     while(encoder1.get_cm()<25) wait_ms(50);
     car.stop();
     wait(0.5);
 
     car.turn(100,-0.3);
     status_log = "turn Right";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     encoder0.reset();
-    while(encoder0.get_cm()<1.5*10.2*2*PI/4) wait_ms(50);
+    while(encoder0.get_cm()<1.8*10.2*2*PI/4) wait_ms(50);
     car.stop();
 
     car.goStraight(100);
     encoder1.reset();
     status_log = "go Straight";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     while(encoder1.get_cm()<75) wait_ms(50);
     car.stop();
     wait(0.5);
@@ -112,99 +135,108 @@ int main(){
 
     car.turn(100,-0.3);
     status_log = "turn Right";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     encoder0.reset();
     while(encoder0.get_cm()<1.5*10.2*2*PI/4) wait_ms(50);
     car.stop();
 
     car.goStraight(100);
     status_log = "go Straight";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     encoder1.reset();
     while(encoder1.get_cm()<125) wait_ms(50);
     car.stop();
+    wait(0.5);
 
     car.turn(100,-0.3);
     status_log = "turn Right";
-    xbee.printf("%s\r\n", status_log);
-    encoder0.reset();
-    while(encoder0.get_cm()<1.5*10.2*2*PI/4) wait_ms(50);
-    car.stop();
-
-    car.goStraight(100);
-     status_log = "go Straight";
-    xbee.printf("%s\r\n", status_log);
-    encoder1.reset();
-    while(encoder1.get_cm()<50) wait_ms(50);
-    car.stop();
-
-    car.turn(100,-0.3);
-    status_log = "turn Right";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     encoder0.reset();
     while(encoder0.get_cm()<1.5*10.2*2*PI/4) wait_ms(50);
     car.stop();
 
     car.goStraight(100);
     status_log = "go Straight";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     encoder1.reset();
-    while(encoder1.get_cm()<25) wait_ms(50);
+    while(encoder1.get_cm()<50) wait_ms(50);
     car.stop();
+    wait(0.5);
 
     car.turn(100,-0.3);
     status_log = "turn Right";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
+    encoder0.reset();
+    while(encoder0.get_cm()<1.5*10.2*2*PI/4) wait_ms(50);
+    car.stop();
+
+    car.goStraight(100);
+    status_log = "go Straight";
+    //xbee.printf("%s\r\n", status_log);
     encoder1.reset();
-    while(encoder1.get_cm()<1.9*10.2*2*PI/4) wait_ms(50);
+    while(encoder1.get_cm()<25) wait_ms(50);
+    car.stop();
+    wait(0.5);
+
+    car.turn(100,-0.3);
+    status_log = "turn Right";
+    //xbee.printf("%s\r\n", status_log);
+    encoder0.reset();
+    while(encoder0.get_cm()<1.9*10.2*2*PI/4) wait_ms(50);
     car.stop();
 
     //scan
-    wait(3);
+    
     status_log = "Identify Item";
-    xbee.printf("%s\r\n", status_log);
+    wait(3);
+    //xbee.printf("%s\r\n", status_log);
 
     car.turn(100,-0.3);
     status_log = "turn Right";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     encoder0.reset();
     while(encoder0.get_cm()<1.5*10.2*2*PI/4) wait_ms(50);
     car.stop();
 
     car.goStraight(100);
     status_log = "go Straight";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     encoder1.reset();
     while(encoder1.get_cm()<25) wait_ms(50);
     car.stop();
+    wait(0.5);
 
     car.turn(100,-0.3);
     status_log = "turn Right";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     encoder0.reset();
     while(encoder0.get_cm()<1.5*10.2*2*PI/4) wait_ms(50);
     car.stop();
 
     car.goStraight(100);
     status_log = "go Straight";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     encoder1.reset();
     while(encoder1.get_cm()<50) wait_ms(50);
     car.stop();
+    wait(0.5);
 
     car.turn(100,-0.3);
     status_log = "turn Right";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     encoder0.reset();
     while(encoder0.get_cm()<1.5*10.2*2*PI/4) wait_ms(50);
     car.stop();
 
     car.goStraight(100);
     status_log = "go Straight";
-    xbee.printf("%s\r\n", status_log);
+    //xbee.printf("%s\r\n", status_log);
     encoder1.reset();
     while(encoder1.get_cm()<125) wait_ms(50);
     car.stop();
+    wait(0.5);
 
-    return 0;
+    status_log = "End";
+    xbee.printf("%s\r\n", status_log);
+*/   
 }
--- final/mqtt_host.py
+++ final/mqtt_host.py
@@ -1,33 +1,33 @@
 import paho.mqtt.client as paho
-import serial
 import time
-
-serdev = '/dev/ttyUSB0'
-s = serial.Serial(serdev, 9600)
-
 
 mqttc = paho.Client()
 
-
-# Settings for connection
-# TODO: revise host to your ip
 host = "localhost"
 topic = "BBcar"
 
-data = []
+port = 1883
+
+
 # Callbacks
 def on_connect(self, mosq, obj, rc):
-      print("Connected rc: " + str(rc))
+    print("Connected rc: " + str(rc))
 
 def on_message(mosq, obj, msg):
-      print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n")
-
-
+    print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n");
+    if msg.topic == "AccX":
+        x.append(float(msg.payload))
+    elif msg.topic == "AccY":
+        y.append(float(msg.payload))
+    elif msg.topic == "AccZ":
+        z.append(float(msg.payload))
+    elif msg.topic == "tilt":
+        tilt.append(float(msg.payload))
 def on_subscribe(mosq, obj, mid, granted_qos):
-      print("Subscribed OK")
+    print("Subscribed OK")
 
 def on_unsubscribe(mosq, obj, mid, granted_qos):
-      print("Unsubscribed OK")
+    print("Unsubscribed OK")
 
 # Set callbacks
 mqttc.on_message = on_message
@@ -40,10 +40,5 @@ print("Connecting to " + host + "/" + topic)
 mqttc.connect(host, port=1883, keepalive=60)
 mqttc.subscribe(topic, 0)
 
-
 while True:
-    line = s.readline()
-    #print(line)
-    mqttc.publish(topic, line)
-    if line == "End"
-        break
+    mqttc.loop()
